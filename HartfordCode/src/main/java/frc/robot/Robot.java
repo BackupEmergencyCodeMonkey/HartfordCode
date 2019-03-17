ad9129a84ba05843f397a59a6b54e2feb899dd3b
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-03-17 15:04:25 -0400

    Formatted

--- HartfordCode/src/main/java/frc/robot/Robot.java
+++ HartfordCode/src/main/java/frc/robot/Robot.java
@@ -23,30 +23,24 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+  
   }
-
   @Override
   public void autonomousInit() {
   }
-
   @Override
   public void autonomousPeriodic() {
   }
-
   @Override
   public void teleopInit() {
   }
-
   @Override
   public void teleopPeriodic() {
   }
-
   @Override
   public void testInit() {
   }
-
   @Override
   public void testPeriodic() {
   }
-
 }
